495e23833c7f28dabd4a67bb6f6a72140809b2a9
Daniel Kaspo <304269+NessDan@users.noreply.github.com>
2019-06-06 18:17:15 -0400

    Force the things so it's easier to flash

--- Arduino/flash-original-firmware.sh
+++ Arduino/flash-original-firmware.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo dfu-programmer atmega16u2 erase
-sudo dfu-programmer atmega16u2 flash Arduino-COMBINED-dfu-usbserial-atmega16u2-Uno-Rev3.hex
+sudo dfu-programmer atmega16u2 erase --force
+sudo dfu-programmer atmega16u2 flash Arduino-COMBINED-dfu-usbserial-atmega16u2-Uno-Rev3.hex --suppress-bootloader-mem
 sudo dfu-programmer atmega16u2 reset
